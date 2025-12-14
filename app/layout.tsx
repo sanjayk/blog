2a41335b363852fd41b2cae0bd7bedd8d3f7b6a6
sanjay <kotagiris@gmail.com>
2025-12-14 18:21:52 +0530

    feat: added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 //import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
@@ -40,6 +41,7 @@ export default function RootLayout({
       <body className={`font-sans antialiased`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-KHM9HXXRPP" />
     </html>
   )
 }
